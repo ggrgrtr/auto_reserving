7aa6db697fd56174466cc7546abb0da5c7b8c748
ggrgrtr <157498741+ggrgrtr@users.noreply.github.com>
2025-10-09 22:01:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ files = reses.dir_i(MotherPath, generalReserveDir_name, copy_from, calendar,
                     count)  # СОЗДАНИЕ ТЕКУЩЕГО РЕЗЕРВА ЧЕРЕЗ ЭКЗЕМПЛЯРА КЛАССА 
 files.reserve_i()  # создание резерва в директории current_backup
 
-exc=files.to_saveData(datas)  # сохранение данных о резервах
+exc = files.to_saveData(datas)  # сохранение данных о резервах
 
 if exc!=True: # ЕСЛИ ДАННЫЕ О НОВОМ РЕЗЕРВЕ НЕ СОХРАНИЛИСЬ -> ОКНО ОШИБКИ
     window.window_Err(exc)
